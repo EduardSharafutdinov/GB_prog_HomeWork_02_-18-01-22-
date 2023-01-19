821d5d2188345f68a694fa7e21b21deb99c06586
Eduard <eduard.sher1@gmail.com>
2023-01-19 10:43:42 +0300

    Zadanie01_HW_18-01 Дано число обозначающее день недели. Выяснить является номер дня недели выходным

--- Zadanie01_HW_18-01/Program.cs
+++ Zadanie01_HW_18-01/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+Console.WriteLine("Ввдите номер дня недели от 1 до 7, для проверки выходного дня: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA > 7) Console.WriteLine("Интересно, но в неделе только 7 дней. Давайте попробуем еще раз! Нужно будет ввести цифру от 1 до 7.");
+
+if (8 > numberA) if (numberA > 5) 
+{
+    Console.WriteLine("Да! Это выходной день!");
+}
+if (6 > numberA) if (numberA > 0)
+{
+    Console.WriteLine("Этот день недели, является будним!");
+}
